fea6880c6fc2d41fa04cbed96c5baafb6790f3be
sanne-oosterhof <91845885+sanne-oosterhof@users.noreply.github.com>
2023-11-03 13:32:18 +0100

    Titel animatie + geluid button + tekstwolkje

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,21 @@
-// JavaScript Document
 console.log("Howdy!");
+
+var geluidButton = document.querySelector("header section button");
+var geluidImg = document.querySelector("header section button img");
+var geluid = document.querySelector("header section audio");
+
+var isPlaying = false;
+
+geluidButton.onclick = smurfIntro;
+
+function smurfIntro() {
+  if (isPlaying) {
+    geluid.pause();
+    geluidImg.src = "images/muzieksmurf.png";
+  } else {
+    geluid.play();
+    geluidImg.src = "images/muzieksmurfActief.png";
+  }
+
+  isPlaying = !isPlaying;
+}
