d1ed2fe45113209b5dc705475ca62a5debbaa940
sanne-oosterhof <91845885+sanne-oosterhof@users.noreply.github.com>
2023-11-04 15:47:13 +0100

    Toggle + darkmode

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,9 @@ var geluidButton = document.querySelector("header section button");
 var geluidImg = document.querySelector("header section button img");
 var geluid = document.querySelector("header section audio");
 
+var darkmodeToggle = document.querySelector("header section:nth-of-type(2) label");
+var body = document.querySelector("body");
+
 var speelAf = false;
 // Voor regel 7, 13 en 21 bron ChatGPT, voor prompt zie miro
 
@@ -20,3 +23,11 @@ function smurfIntro() {
 
   speelAf = !speelAf;
 }
+
+
+
+// darkmodeToggle.addEventListener("click", switchTheme);
+
+// function switchTheme(){
+//   body.classList.toggle("darkmode");
+// }
