bfa4d49e4fb98d4dd6b08da1a456172c37c09da6
sanne-oosterhof <91845885+sanne-oosterhof@users.noreply.github.com>
2023-11-06 15:46:42 +0100

    Darkmode en 'smurfen' woord gefixt

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,25 @@
 console.log("Howdy!");
 
+// variabele voor body 
 var body = document.querySelector("body");
 
+// variabelen voor muziekbutton
+// Voor regel 11, 20 en 30 bron ChatGPT, voor prompt zie miro
 var geluidButton = document.querySelector("header section button");
 var geluidImg = document.querySelector("header section button img");
 var geluid = document.querySelector("header section audio");
+var speelAf = false;
 
+// variabelen voor darkmode 
+var darkmodeTekst = document.querySelector("header section:nth-of-type(2) p span")
 var darkmodeToggle = document.querySelector("header section label input[type='checkbox']");
 var backgroundImage = document.querySelector("main ol.background-light");
 
-var darkmodeTekst = document.querySelector("header section:nth-of-type(2) p span")
-
-
-var speelAf = false;
-// Voor regel 7, 13 en 21 bron ChatGPT, voor prompt zie miro
-
+// Voor muziek button 
 geluidButton.onclick = smurfIntro;
+darkmodeToggle.addEventListener("change", switchTheme);
 
+// functie voor muziekbutton 
 function smurfIntro() {
   if (speelAf) {
     geluid.pause();
@@ -28,15 +31,11 @@ function smurfIntro() {
   speelAf = !speelAf;
 }
 
-
-
-darkmodeToggle.addEventListener("change", switchTheme);
-
-
+// Functie voor darkmode en alles wat daarmee verandert 
 function switchTheme(){
-  body.classList.toggle("darkmode");
+  document.documentElement.classList.toggle("darkmode");
   backgroundImage.classList.toggle("background-dark");
-  
+
   // bron voor tekst toggle: https://www.w3schools.com/howto/howto_js_toggle_text.asp 
   if (darkmodeToggle.checked) {
     darkmodeTekst.textContent = "Aan";
