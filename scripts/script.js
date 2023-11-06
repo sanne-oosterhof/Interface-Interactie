0a56bbf96f90c2182a85e7be526b27afa3ba7599
sanne-oosterhof <91845885+sanne-oosterhof@users.noreply.github.com>
2023-11-06 11:47:51 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,14 @@
 console.log("Howdy!");
 
+var body = document.querySelector("body");
+
 var geluidButton = document.querySelector("header section button");
 var geluidImg = document.querySelector("header section button img");
 var geluid = document.querySelector("header section audio");
 
-var darkmodeToggle = document.querySelector("header section:nth-of-type(2) label");
-var body = document.querySelector("body");
+var darkmodeToggle = document.querySelector("header section label input[type='checkbox']");
+var backgroundImage = document.querySelector("main ol.background-light");
+
 
 var speelAf = false;
 // Voor regel 7, 13 en 21 bron ChatGPT, voor prompt zie miro
@@ -20,14 +23,15 @@ function smurfIntro() {
     geluid.play();
     geluidImg.src = "images/muzieksmurfActief.png";
   }
-
   speelAf = !speelAf;
 }
 
 
 
-// darkmodeToggle.addEventListener("click", switchTheme);
+darkmodeToggle.addEventListener("change", switchTheme);
 
-// function switchTheme(){
-//   body.classList.toggle("darkmode");
-// }
+
+function switchTheme(){
+  body.classList.toggle("darkmode");
+  backgroundImage.classList.toggle("background-dark");
+}
